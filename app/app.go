ae8ebeafa9da54e12d97d087e2925f662be89e8b
Leyuan Pan <leyuanpan@gmail.com>
2026-03-16 20:08:41 -0700

    initialization

---
+++ app/app.go
@@ -0,0 +1,8 @@
+package app
+
+import "context"
+
+func Init(ctx context.Context) error {
+
+	return nil
+}
---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/pancpp/peanut-disc/app"
+	"github.com/pancpp/peanut-disc/conf"
+	"github.com/pancpp/peanut-disc/logger"
+)
+
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// config
+	if err := conf.Init(); err != nil {
+		log.Fatal(err)
+	}
+
+	// logger
+	if err := logger.Init(); err != nil {
+		log.Fatal(err)
+	}
+
+	// say hello
+	log.Println("Hello, peanut discovery server!")
+
+	// app
+	if err := app.Init(ctx); err != nil {
+		log.Fatal(err)
+	}
+
+	// wait for keyboard interruption
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGTERM, syscall.SIGINT, syscall.SIGHUP)
+	<-sigChan
+
+	// say goodbye
+	log.Println("Goodbye!")
+}
